a76ed81b71cd2c187840961e4e0171d0e2dfc44b
kofimokome <kofimokome10@gmail.com>
2021-02-02 16:47:23 +0100

    update advanced settings view file

--- includes/views/settings/advanced.php
+++ includes/views/settings/advanced.php
@@ -4,7 +4,7 @@ namespace kmcf7_message_filter;
 ?>
     <h2>Advanced Settings </h2>
     These settings will work only when auto delete is activated
-    <h2 style="color: red">Note: These are experimental settings. Please <a href="https://github.com/kofimokome/cf7-message-filter/discussions/9" target="_blank">submit any feedback here</a></h2>
+    <h2 style="color: red">Note: These are experimental features. Please <a href="https://github.com/kofimokome/cf7-message-filter/discussions/9" target="_blank">submit any feedback here</a></h2>
     <?php settings_errors(); ?>
     <form method="post" action="options.php">
         <?php
