f1f9165782d80334b374c3fb7e7a224d825a3d3d
kofimokome <kofimokome10@gmail.com>
2022-08-21 05:32:56 +0100

    Update version to 1.4.0, fix syntax issues

--- modules/settings/templates/extensions.php
+++ modules/settings/templates/extensions.php
@@ -1,32 +1,30 @@
 <?php
 
 namespace kmcf7_message_filter;
-$extensions = apply_filters( 'kmcf7_extensions', [] );
+$extensions       = apply_filters( 'kmcf7_extensions', array() );
 $link_to_messages = admin_url( 'admin.php' ) . '?page=kmcf7-message-filter-options&tab=messages';
 
 ?>
-    <h2><?php _e( "Extensions", KMCF7MS_TEXT_DOMAIN ) ?> </h2>
+<h2><?php _e( "Extensions", KMCF7MS_TEXT_DOMAIN ) ?> </h2>
 
-	<?php _e( "The following extensions are available:", KMCF7MS_TEXT_DOMAIN ) ?>
-    <div class="card">
-        <table class="table table-striped">
-            <tr>
-                <td>
-                    <h2><?php _e( "Hide Error Messages (Free Trial)", KMCF7MS_TEXT_DOMAIN ) ?></h2>
-					<?php _e( "This extension hides the error message and shows a success message if a spam is found in the submitted form", KMCF7MS_TEXT_DOMAIN ) ?>
-                </td>
-                <td>
-					<?php if ( in_array( 'hide_error_messages', $extensions ) ): ?>
-                        <a class="button"
-                           href="<?php echo $link_to_messages ?>"><?php _e( "Configure", KMCF7MS_TEXT_DOMAIN ) ?></a>
-					<?php else: ?>
-                        <a class="button"
-                           href="https://kofimokome.stream"
-                           target="_blank"><?php _e( "Add Extension", KMCF7MS_TEXT_DOMAIN ) ?></a>
-					<?php endif; ?>
-                </td>
-            </tr>
-        </table>
-    </div>
-
-<?php
+<?php _e( "The following extensions are available:", KMCF7MS_TEXT_DOMAIN ) ?>
+<div class="card">
+    <table class="table table-striped">
+        <tr>
+            <td>
+                <h2><?php _e( "Hide Error Messages (Free Trial)", KMCF7MS_TEXT_DOMAIN ) ?></h2>
+				<?php _e( "This extension hides the error message and shows a success message if a spam is found in the submitted form", KMCF7MS_TEXT_DOMAIN ) ?>
+            </td>
+            <td>
+				<?php if ( in_array( 'hide_error_messages', $extensions ) ) { ?>
+                    <a class="button"
+                       href="<?php echo $link_to_messages ?>"><?php _e( "Configure", KMCF7MS_TEXT_DOMAIN ) ?></a>
+				<?php } else { ?>
+                    <a class="button"
+                       href="https://kofimokome.stream"
+                       target="_blank"><?php _e( "Add Extension", KMCF7MS_TEXT_DOMAIN ) ?></a>
+				<?php } ?>
+            </td>
+        </tr>
+    </table>
+</div>
